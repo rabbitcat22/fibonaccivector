baa3b2e31ab29afccae4f1045e99b17533038f4b
rabbitcat22 <rem22153@gmail.com>
2019-10-04 20:06:14 +0900

    fibonacci

---
+++ ream.cpp
@@ -0,0 +1,22 @@
+#include <lostream>
+#include <vector>
+using namespace std;
+vector<std::pair<int, int>> vt;
+
+int main(void){
+
+vt.emplace_back(pair<int, int>(1, 0));
+vt.emplace_back(pair<int, int>(0, 1));
+
+for (int 1-2; 1<41; i++) {
+    vt.emplace_back(pair<int, int>)
+    (vt[1-2].first + vt[1-1].first,
+     vt[1-2].second + vt[1-1].second));
+}
+int T,A;
+cin >> T;
+while (T--) {
+    cin >> A;
+    cout << vt[A].first << ' ' << vt[A].second << '\n';
+}
+}
